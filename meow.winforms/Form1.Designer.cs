550ebba0778b797f89aa6a2398fda898c9e62de3
taisiiasun01-afk <taisiia.sun01@gmail.com>
2026-09-16 13:33:03 +0700

    Implemented WinForms UI for Cat entity

--- meow.winforms/Form1.Designer.cs
+++ meow.winforms/Form1.Designer.cs
@@ -28,13 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.IbTitle = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtBreed = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnGroup = new System.Windows.Forms.Button();
+            this.btnHeavy = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IbTitle
+            // 
+            this.IbTitle.AutoSize = true;
+            this.IbTitle.Location = new System.Drawing.Point(354, 13);
+            this.IbTitle.Name = "IbTitle";
+            this.IbTitle.Size = new System.Drawing.Size(79, 16);
+            this.IbTitle.TabIndex = 0;
+            this.IbTitle.Text = "блаблабла";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(368, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 1;
+            this.txtName.Text = "Кличка";
+            // 
+            // txtBreed
+            // 
+            this.txtBreed.Location = new System.Drawing.Point(367, 93);
+            this.txtBreed.Name = "txtBreed";
+            this.txtBreed.Size = new System.Drawing.Size(100, 22);
+            this.txtBreed.TabIndex = 2;
+            this.txtBreed.Text = "Порода";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(367, 140);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 22);
+            this.txtAge.TabIndex = 3;
+            this.txtAge.Text = "Возраст";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(367, 182);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 22);
+            this.txtWeight.TabIndex = 4;
+            this.txtWeight.Text = "Вес";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(368, 223);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 22);
+            this.txtColor.TabIndex = 5;
+            this.txtColor.Text = "Окрас";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(51, 300);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(139, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(239, 300);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(171, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Обновить";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Кличка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Порода";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Возраст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Вес";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(183, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Окрас";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(469, 300);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(139, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(649, 300);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(139, 23);
+            this.btnShowAll.TabIndex = 14;
+            this.btnShowAll.Text = "Показать все";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnGroup
+            // 
+            this.btnGroup.Location = new System.Drawing.Point(167, 374);
+            this.btnGroup.Name = "btnGroup";
+            this.btnGroup.Size = new System.Drawing.Size(139, 23);
+            this.btnGroup.TabIndex = 15;
+            this.btnGroup.Text = "Группировка по породе";
+            this.btnGroup.UseVisualStyleBackColor = true;
+            this.btnGroup.Click += new System.EventHandler(this.btnGroup_Click);
+            // 
+            // btnHeavy
+            // 
+            this.btnHeavy.Location = new System.Drawing.Point(412, 374);
+            this.btnHeavy.Name = "btnHeavy";
+            this.btnHeavy.Size = new System.Drawing.Size(139, 23);
+            this.btnHeavy.TabIndex = 16;
+            this.btnHeavy.Text = "Тяжелее N кг";
+            this.btnHeavy.UseVisualStyleBackColor = true;
+            this.btnHeavy.Click += new System.EventHandler(this.btnHeavy_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(548, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnHeavy);
+            this.Controls.Add(this.btnGroup);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtBreed);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.IbTitle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label IbTitle;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtBreed;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnGroup;
+        private System.Windows.Forms.Button btnHeavy;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- meow.winforms/Form1.cs
+++ meow.winforms/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using meow.core.Logic;
+using meow.core.Models;
 
 namespace meow.winforms
 {
@@ -16,5 +18,125 @@ namespace meow.winforms
         {
             InitializeComponent();
         }
+        private CatLogic catLogic = new CatLogic();
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtName.Text;
+                string breed = txtBreed.Text;
+                int age = int.Parse(txtAge.Text);
+                double weight = double.Parse(txtWeight.Text);
+                string color = txtColor.Text;
+
+                catLogic.AddCat(name, breed, age, weight, color);
+                RefreshGrid();
+                ClearFields();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Возраст — целое число, вес — число с точкой (например, 5.2)");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Выдели кота в таблице!");
+                return;
+            }
+
+            try
+            {
+                var selected = (Cat)dataGridView1.CurrentRow.DataBoundItem;
+                catLogic.UpdateCat(selected.Id,
+                    txtName.Text, txtBreed.Text,
+                    int.Parse(txtAge.Text),
+                    double.Parse(txtWeight.Text),
+                    txtColor.Text);
+
+                RefreshGrid();
+                ClearFields();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Проверь формат ввода!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Выдели кота!");
+                return;
+            }
+
+            var selected = (Cat)dataGridView1.CurrentRow.DataBoundItem;
+            catLogic.DeleteCat(selected.Id);
+            RefreshGrid();
+            ClearFields();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            RefreshGrid();
+
+        }
+
+        private void btnGroup_Click(object sender, EventArgs e)
+        {
+            var groups = catLogic.GroupByBreed();
+            string result = "";
+            foreach (var g in groups)
+            {
+                result += $"Порода: {g.Key} ({g.Value.Count} шт.)\n";
+                foreach (var cat in g.Value)
+                    result += "   " + cat + "\n";
+                result += "\n";
+            }
+            MessageBox.Show(result, "Группировка по породе");
+        }
+
+        private void btnHeavy_Click(object sender, EventArgs e)
+        {
+            string input = txtHeavyWeight.Interaction.InputBox(
+       "Введите минимальный вес (кг):", "Поиск тяжёлых котов", "5");
+            if (double.TryParse(input, out double minWeight))
+            {
+                var heavy = catLogic.GetCatsHeavierThan(minWeight);
+                string result = heavy.Count == 0
+                    ? "Нет котов тяжелее " + minWeight
+                    : string.Join("\n", heavy);
+                MessageBox.Show(result, "Тяжёлые коты");
+            }
+        }
+        private void RefreshGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = catLogic.GetAllCats();
+        }
+
+        private void ClearFields()
+        {
+            txtName.Text = "";
+            txtBreed.Text = "";
+            txtAge.Text = "";
+            txtWeight.Text = "";
+            txtColor.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
     }
+
 }
